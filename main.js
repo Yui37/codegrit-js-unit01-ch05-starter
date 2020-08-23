904e17c6e58f5421e249510e6215d1e7f975fb8d
Yui <ytkakentan@yahoo.co.jp>
2020-08-23 20:40:10 +0900

    first add

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
+'use strict';
+
+function keysColor(){
+    const keyColor =document.getElementsByClassName('key');
+    for (let key of keyColor) {
+        key.classList.add('playing')
+    }
+}
+
+keysColor();
+
+function keysColorReset(){
+    const keyColor =document.getElementsByClassName('key');
+    for (let key of keyColor) {
+        key.classList.remove('playing')
+    }
+}
+
+keysColorReset();
+
+const keysAction = document.querySelectorAll('[deta-key="addStyle"]');
+
+for (let kbd of keysAction){
+    kbd.addEventListener('click' , evt => {
+        evt.keysDefault();
+        keysColor();
+    });
+}
+
+
+const keysResetAction = document.querySelectorAll('[deta-key="resetStyle"]');
+
+for (let kbd of keysResetAction){
+    kbd.addEventListener('click' , evt => {
+        evt.keysDefault();
+        keysColorReset();
+    });
+}
+
+
+
 
