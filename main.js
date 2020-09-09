723e262340b53fc2b0ab030ec8084346daec4b33
Yui <ytkakentan@yahoo.co.jp>
2020-09-09 16:55:34 +0900

    add

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ function addRedFrame(e){
     const elements = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     console.log(elements);
     elements.classList.add('playing');
-}
+
     // 押されたキーボード番号を取得する　使う技法　event.keyCode DOM
     // 押されたキーボードにはクラス属性playingを付与する。 classList.add('playing')
-
+}
 window.addEventListener('keydown', addRedFrame);
 
-// function resetRedFrame(e){
-//     const resetRed = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-//     elements.classList.remove('playing');
-// }
+function resetRedFrame(e){
+    const resetRed = document.querySelector(`.key.playing[data-key="${e.keyCode}"]`);
+    resetRed.classList.remove('playing');
+}
 
-// window.addEventListener('keyup', addRedFrame);
+window.addEventListener('keyup', resetRedFrame);
