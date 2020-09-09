4c2cdeb126544358f1a6191e7fe1ab113d9dcd9d
Yui <ytkakentan@yahoo.co.jp>
2020-09-09 20:53:39 +0900

    final add

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-function addRedFrame(e){
-    const elements = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    console.log(elements);
-    elements.classList.add('playing');
-
-    // 押されたキーボード番号を取得する　使う技法　event.keyCode DOM
+function addRedFrameAndSound(e){
+    const RedFrame = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    RedFrame.classList.add('playing');
+    const sounds = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    sounds.play();
+}
+   // 押されたキーボード番号を取得する　使う技法　event.keyCode DOM
     // 押されたキーボードにはクラス属性playingを付与する。 classList.add('playing')
-}
-window.addEventListener('keydown', addRedFrame);
+    //押されたキーボードの番号を取得　e.keyCode
+    //キーボードごとに設定された音が鳴る
+window.addEventListener('keydown', addRedFrameAndSound);
 
-function resetRedFrame(e){
-    const resetRed = document.querySelector(`.key.playing[data-key="${e.keyCode}"]`);
-    resetRed.classList.remove('playing');
+function resetRedFrameAndSound(e){
+    const resetRedFrame = document.querySelector(`.key.playing[data-key="${e.keyCode}"]`);
+    resetRedFrame.classList.remove('playing');
+    const resetSounds = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    resetSounds.pause();
 }
 
-window.addEventListener('keyup', resetRedFrame);
+window.addEventListener('keyup', resetRedFrameAndSound);
