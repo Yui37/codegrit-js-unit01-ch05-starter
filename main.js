011ed5e2bff7aa8efb3e3633b0f0b466772f1f13
Yui <ytkakentan@yahoo.co.jp>
2020-09-10 21:12:00 +0900

    final add

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
-function addRedFrameAndSound(e){
-    const RedFrame = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    RedFrame.classList.add('playing');
+function addEffects(e){
+    const redFrame = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    redFrame.classList.add('playing');
     const sounds = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     sounds.play();
 }
-   // 押されたキーボード番号を取得する　使う技法　event.keyCode DOM
-    // 押されたキーボードにはクラス属性playingを付与する。 classList.add('playing')
-    //押されたキーボードの番号を取得　e.keyCode
-    //キーボードごとに設定された音が鳴る
-window.addEventListener('keydown', addRedFrameAndSound);
+window.addEventListener('keydown', addEffects);
 
-function resetRedFrameAndSound(e){
-    const resetRedFrame = document.querySelector(`.key.playing[data-key="${e.keyCode}"]`);
+function removeEffects(e){
+    const resetRedFrame = document.querySelector(`div.key.playing[data-key="${e.keyCode}"]`);
     resetRedFrame.classList.remove('playing');
     const resetSounds = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     resetSounds.pause();
 }
-
-window.addEventListener('keyup', resetRedFrameAndSound);
+window.addEventListener('keyup', removeEffects);
